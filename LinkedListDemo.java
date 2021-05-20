4ab20caa1fae6585faa65db6ce8df90a3059ddec
pvijaykrishna549 <you@example.com>
2021-05-20 15:01:22 +0530

    Insertion of Number 30 and 40 at desigred places

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -12,8 +12,11 @@ class Node {
 
         LinkedListDemo llist = new LinkedListDemo();
         llist.Insert(56);
-        llist.Insert(30);
+      //  llist.Insert(30);
         llist.Insert(70);
+
+        llist.InsertAt(1,30);
+        llist.InsertAt(2,40);
 
         llist.show();
 
@@ -45,6 +48,24 @@ public class LinkedListDemo {
             n.next = node;
         }
     }
+    public void InsertAt(int index, int data)
+    {
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+
+        Node n = head;
+        for (int i =0; i<index-1;i++)
+        {
+            n = n.next;
+        }
+        node.next = n.next;
+        n.next = node;
+
+
+    }
+
+
     public void show()
     {
         Node node = head;
